4b03a914e605b13b81a55ce0cea26f7bca43efde
Brianino <brian.manganaro@gmail.com>
2017-10-21 12:21:07 +0100

    Updated gitignore, added example config file

---
+++ config example.js
@@ -0,0 +1,12 @@
+var config = {}
+
+/*
+  *	Config Example
+  *	Create a folder called "Files" within the Emote-Bro directory.
+  *	Rename this file to config.js
+*/
+config.token = ''/*String for Bot token*/;
+config.owner = ''/*Owner ID*/;
+config.password = ''/*Encryption password*/
+
+module.exports = config;
---
+++ spectre.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+
+//require command manager
+//require automation manager
+
