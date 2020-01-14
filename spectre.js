44e6a6198da6532211c5fb199d819c454e1b9823
Brianino <brian.manganaro@gmail.com>
2020-01-14 20:41:04 +0000

    wiping last few files

--- spectre.js
+++ spectre.js
@@ -1,8 +1,4 @@
 "use strict";
-<<<<<<< HEAD
-const color = require('colors');
-=======
->>>>>>> Starting from scratch
 const Discord = require('discord.js');
 const config = require('./config.json');
 
