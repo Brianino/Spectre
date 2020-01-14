b81d64cc80264454397c49adbbd67e4ec08eff17
Brianino <brian.manganaro@gmail.com>
2020-01-14 23:57:50 +0000

    added module loader (wip)

--- spectre.js
+++ spectre.js
@@ -1,8 +1,30 @@
 "use strict";
+if (!process.env.DEBUG) process.env.DEBUG = '*:log,*:info,*:warn,*:error';
+const modules = require('./etc/moduleLoader.js');
+const log = require('debug-logger')('main');
 const Discord = require('discord.js');
-const config = require('./config.json');
+const time = require('./etc/time.js');
+const fs = require('fs');
+
+try {
+	const {token, ...config} = require('./config.json');
+} catch (e) {
+	fs.writeFileSync('./config.json', JSON.stringify({
+		token: ""
+	}));
+	log.log('config.json file created');
+	process.exit();
+}
 
 const bot = new Discord.Client();
 
+bot.on('ready', () => {
+	log.info(time(), 'Bot ready');
+});
 
-bot.login(config.token); //Bot Token
+bot.on('error', e => {
+	log.error(time(), e.toString());
+	log.debug(e.stack);
+})
+
+//bot.login(token); //Bot Token
