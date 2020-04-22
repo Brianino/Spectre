152e5f9a32d12d767d952a84280af4485defb525
Brianino <brian.manganaro@gmail.com>
2020-04-22 22:27:39 +0100

    userinfo command added

---
+++ modules/userinfoModule.js
@@ -0,0 +1,66 @@
+const log = require('debug-logger')('userinfo-module');
+const time = require('../etc/time.js');
+
+setupModule(function () {
+	this.command = 'userinfo';
+	this.description = 'Displays info on a user';
+	this.guildOnly = true;
+
+	this.exec(async (msg, input) => {
+		let mention = /(?<=\<@!?)\d{17,19}(?=\>)/.exec(input = String(input)) || [],
+			user = msg.guild.member(mention[0] || input) || msg.member, embed, flags;
+
+		embed = {
+			title: user.displayName,
+			description: user.user.tag || 'Can\'t get user tag',
+			color: user.displayColor,
+			thumbnail: {
+				url: user.user.displayAvatarURL({
+					format: 'png',
+					dynamic: true,
+				}),
+			},
+			fields: [{
+				name: 'Created',
+				value: [
+					user.user.createdAt.toDateString(),
+					'at ' + user.user.createdAt.toTimeString(),
+				].join('\n'),
+				//inline: true,
+			}, {
+				name: 'Joined',
+				value: [
+					user.joinedAt.toDateString(),
+					'at ' + user.joinedAt.toTimeString(),
+				].join('\n'),
+				//inline: true,
+			}, {
+				name: 'Roles',
+				value: '`' + user.roles.cache.map(role => role.name).join('` `') + '`',
+				//inline: true,
+			}, {
+				name: 'Permissions',
+				value: '`' + user.permissions.toArray(false).join('` `') + '`',
+				//inline: true,
+			}],
+			footer: {
+				text: user.presence.status + ' - ' + user.id,
+				icon_url: user.user.displayAvatarURL({
+					format: 'png',
+					dynamic: true,
+				}),
+			}
+		}
+
+		if (user.premiumSince) {
+			embed.fields.push({
+				name: 'Server Boost',
+				value: [
+					user.premiumSince.toDateString(),
+					'at ' + user.premiumSince.toTimeString(),
+				].join('\n'),
+			});
+		}
+		return msg.channel.send({embed});
+	});
+});
