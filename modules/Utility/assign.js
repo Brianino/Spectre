b02e70ff940e4c7f1aa665a405727ed785612af9
Brianino <brian.manganaro@gmail.com>
2022-01-09 23:19:09 +0100

    Added self assign role command
    Added the command assign, which can be used to assign self assignable roles (which can be configured with the configure command).
    Modified the log configuration to append the date suffix without the brackets

---
+++ modules/Utility/assign.js
@@ -0,0 +1,37 @@
+this.description = 'Self assign a role';
+
+this.arguments = '<role>';
+
+addConfig('assign_roles', Set, { default: new Set(), description: 'Self assignable roles', configurable: true });
+
+function inGuild () {
+	const { getRoleID } = Utils;
+
+	function idToMention (id) {
+		return `<@&${id}>`;
+	}
+
+	return async (msg, input) => {
+		const requestedRole = await getRoleID(input, msg.guild, { resolve: true, allowText: 'partial' }), promises = [];
+
+		for (const role of this.config.assign_roles) {
+			promises.push((async () => {
+				const resolvedRole = await getRoleID(role, msg.guild, { resolve: true, allowText: 'partial' });
+
+				if (role !== idToMention(resolvedRole.id)) {
+					this.config.assign_roles.delete(role);
+					this.config.assign_roles.add(idToMention(resolvedRole.id));
+				}
+				if (resolvedRole === requestedRole) {
+					// assign role to user
+					return msg.member.roles.add(resolvedRole).catch(async e => {
+						log.error('Unable to assign role', e);
+						const reply = await msg.channel.send({ message: 'can\'t assign role' });
+						return reply.delete({ timeout: 10000 });
+					});
+				}
+			})());
+		}
+		return Promise.allSettled(promises);
+	};
+}
